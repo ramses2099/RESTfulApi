5522d04e11d2cde458fdf27d2a8ba249173eb3b2
ramses20999 <ramses2099@gmail.com>
2019-02-12 09:09:55 -0400

    handler exception product

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,24 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($request->expectsJson())
+        {
+            if($exception instanceof ModelNotFoundException)
+            {
+                return response()->json([
+                   'errors'=>'Product Model not found'],
+                   Response::HTTP_NOT_FOUND);
+            }
+
+            if($exception instanceof ModelNotFoundException)
+            {
+                return response()->json([
+                   'errors'=>'Incorrect route'],
+                   Response::HTTP_NOT_FOUND);
+            }
+
+        }
+
         return parent::render($request, $exception);
     }
 }
